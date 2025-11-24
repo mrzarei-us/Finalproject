26e2c8176a0aa8bd86756ed143c093b6d9a8eef2
mrzarei-us <mzarei@my.smccd.edu>
2025-11-24 11:57:52 -0800

    Initial Commit

---
+++ FinalProject_MohammadReza_Zarei/FinalProject_MohammadReza_ZareiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_MohammadReza_ZareiApp.swift
+//  FinalProject_MohammadReza_Zarei
+//
+//  Created by Mohammadreza Zarei on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject_MohammadReza_ZareiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinalProject_MohammadReza_ZareiTests/FinalProject_MohammadReza_ZareiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_MohammadReza_ZareiTests.swift
+//  FinalProject_MohammadReza_ZareiTests
+//
+//  Created by Mohammadreza Zarei on 11/24/25.
+//
+
+import Testing
+@testable import FinalProject_MohammadReza_Zarei
+
+struct FinalProject_MohammadReza_ZareiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
